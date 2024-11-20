867cf728617f7992f56630d2bfe23321fd45cf47
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-20 13:34:38 -0600

    Create Temp.js

---
+++ Temp.js
@@ -0,0 +1,12 @@
+// converts inputted fahrenheit value to celsius
+function toC(F){
+// (32°F − 32) × 5/9 = 0°C to celsius formula
+  let toC1 = F - 32;
+  let productC = toC1 * 5/9;
+  return productC;
+};
+
+function toF(C){
+// (0°C + 32) / 5/9 = 32°C presumably the to fahrenheit formula (idk i just reversed the operations
+  
+};
